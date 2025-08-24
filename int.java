567f74d40d9c413219d02428decbfa38070d0b1e
Praveen kumar.M <praveenkumar20026@gmail.com>
2025-08-24 19:49:31 +0530

    Create int.java

---
+++ int.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        int sum = a + b;
+        int diff = a - b;
+        int prod = a * b;
+        int div = (b != 0) ? (a / b) : 0;  // Avoid divide by zero
+        int mod = (b != 0) ? (a % b) :
+        System.out.println("\nResults:");
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + diff);
+        System.out.println("Multiplication: " + prod);
+        if (b != 0) {
+            System.out.println("Division: " + div);
+            System.out.println("Modulus: " + mod);
+        } else {
+            System.out.println("Division and Modulus not possible (denominator is 0)");
+        }
+        sc.close();
+    }
+}
